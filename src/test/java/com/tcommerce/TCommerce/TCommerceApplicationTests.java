e53da5735c8d7ab5e9ffd746cfd23459063e519b
Alejandro Quicaño <alejandroxd25k@gmail.com>
2026-01-26 13:41:59 -0500

    imp: add spring files

---
+++ src/test/java/com/tcommerce/TCommerce/TCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcommerce.TCommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TCommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
