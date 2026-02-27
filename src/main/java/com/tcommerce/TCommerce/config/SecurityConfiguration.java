8d995f327ec8e34325338e1aa8673788d04da017
Alejandro Quicaño <alejandroxd25k@gmail.com>
2026-02-26 21:34:21 -0500

    imp: add swagger basic documentation

--- src/main/java/com/tcommerce/TCommerce/config/SecurityConfiguration.java
+++ src/main/java/com/tcommerce/TCommerce/config/SecurityConfiguration.java
@@ -50,17 +50,17 @@ public class SecurityConfiguration {
                                         "/graphql/**",
                                         "/graphiql",
                                         "/graphiql/**",
-                                        "/favicon.ico"
-                                        //"/v2/api-docs",
-                                        //"/v3/api-docs",
-                                        //"/v3/api-docs/**",
-                                        //"/swagger-resources",
-                                        //"/swagger-resources/**",
-                                        //"/configuration/ui",
-                                        //"/configuration/security",
-                                        //"/swagger-ui/**",
-                                        //"/webjars/**",
-                                        //"/swagger-ui.html"
+                                        "/favicon.ico",
+                                        "/v2/api-docs",
+                                        "/v3/api-docs",
+                                        "/v3/api-docs/**",
+                                        "/swagger-resources",
+                                        "/swagger-resources/**",
+                                        "/configuration/ui",
+                                        "/configuration/security",
+                                        "/swagger-ui/**",
+                                        "/webjars/**",
+                                        "/swagger-ui.html"
 
                                 ).permitAll()
                                 .requestMatchers(HttpMethod.POST, "/api/v1/payments/webhook").permitAll()
